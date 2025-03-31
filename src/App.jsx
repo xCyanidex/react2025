ca1a12693d8c1928468ba591fcbe2f8c72716e28
Duraiz Haider <haiderduraiz@gmail.com>
2025-03-31 19:26:48 -0400

    average calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average,setAverage]=useState(0);
+  const [allRating,setAllRating]=useState(0);
 
   const ratingHandler=(rating)=>{
     if(rating=='good'){
-        setGood(good+1);
+        setGood(prevState=>prevState+1);
     }else if(rating=='neutral'){
-setNeutral(neutral+1);
+setNeutral((prevState) => prevState + 1);
     }else if(rating=='bad'){
-setBad(bad+1);
+setBad((prevState) => prevState + 1);
     }
   }
+
+  useEffect(()=>{
+    if(good>0){
+let average = (good - bad) / (good + bad + neutral);
+setAverage(average);
+setAllRating(good + bad + neutral);
+    }
+
+  },[good,bad,neutral])
+
 
   return (
     <>
       <h1 style={{ color: "white" }}>Give Feedback</h1>
-      <div style={{display:"flex",justifyContent:"space-evenly"}}>
-        <button style={{}} onClick={() => ratingHandler("good")}>Good</button>
-        <button style={{}} onClick={() => ratingHandler("neutral")}>Neutral</button>
-        <button style={{}} onClick={() => ratingHandler("bad")}>Bad</button>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <button style={{}} onClick={() => ratingHandler("good")}>
+          Good
+        </button>
+        <button style={{}} onClick={() => ratingHandler("neutral")}>
+          Neutral
+        </button>
+        <button style={{}} onClick={() => ratingHandler("bad")}>
+          Bad
+        </button>
       </div>
       <h2 style={{ color: "white" }}>Statistics</h2>
       <p style={{ color: "white" }}>Good: {good}</p>
       <p style={{ color: "white" }}>Neutral: {neutral}</p>
-      <p style={{ color: "white" }} >Bad: {bad}</p>
+      <p style={{ color: "white" }}>Bad: {bad}</p>
+      <p style={{ color: "white" }}>all: {allRating}</p>
+      <p style={{ color: "white" }}>Average: {average}</p>
     </>
   ); 
 };
