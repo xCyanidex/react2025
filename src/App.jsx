06082dc400b0077b7f6f673f2d904df05a58bc43
Duraiz Haider <haiderduraiz@gmail.com>
2025-04-03 17:53:23 -0400

    added voting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import Stats from "./components/Stats";
 
 const App = () => {
 
+     const [selected, setSelected] = useState(0);
+     const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0,0]);
+
   const randomizer=()=>{
     const randomInt = Math.floor(Math.random() * 8);
     setSelected(randomInt);
   }
 
+  const voter = (selectedIndex) => {
+    const copy = [...votes];
+    copy[selectedIndex]=copy[selectedIndex]+1;
+    setVotes(copy);
+  };
     const anecdotes = [
       "If it hurts, do it more often.",
       "Adding manpower to a late software project makes it later!",
@@ -18,12 +26,14 @@ const App = () => {
       "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
       "The only way to go fast, is to go well.",
     ];
-     const [selected, setSelected] = useState(0);
+  
 
   return (
     <>
       <div style={{ color: "white" }}>{anecdotes[selected]}</div>
+      <h4 style={{ color: "white" }}>This one has {votes[selected]} votes;</h4>
       <button onClick={randomizer}>Next anedote</button>
+      <button onClick={() => voter(selected)}>Vote</button>
     </>
   ); 
 };
