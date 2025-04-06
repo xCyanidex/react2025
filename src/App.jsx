f3f24e5dd11463eac558003cd79f00ffc929cee8
Duraiz Haider <haiderduraiz@gmail.com>
2025-04-05 22:01:25 -0400

    demo for event handler and form state

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,54 @@
-import { useEffect, useState } from "react";
-import Stats from "./components/Stats";
+import { useState } from "react";
+import Note from "./components/Note";
 
-const App = () => {
+const App = (props) => {
 
-     const [selected, setSelected] = useState(0);
-     const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0,0]);
-     const [mostVoted,setMostVoted]=useState();
+  const [notes, setNotes] = useState(props.notes);
 
-  const randomizer=()=>{
-    const randomInt = Math.floor(Math.random() * 8);
-    setSelected(randomInt);
-  }
+  const [newNote, setNewNote] = useState("a new note..."); 
 
-  const voter = (selectedIndex) => {
-    const copy = [...votes];
-    copy[selectedIndex]=copy[selectedIndex]+1;
-    setVotes(copy);
-   let maxIndex = votes.indexOf(Math.max(...votes));
-    setMostVoted(maxIndex);
-  };
-    const anecdotes = [
-      "If it hurts, do it more often.",
-      "Adding manpower to a late software project makes it later!",
-      "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-      "Premature optimization is the root of all evil.",
-      "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-      "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-      "The only way to go fast, is to go well.",
-    ];
-  
+    const [showAll, setShowAll] = useState(true);
+
+ const addNote = (event) => {
+   event.preventDefault();
+   const noteObject = {
+     content: newNote,
+     important: Math.random() < 0.5,
+     id: String(notes.length + 1),  
+   };
+
+   setNotes(notes.concat(noteObject));
+   setNewNote("");
+ };
+
+     const handleNoteChange = (event) => {
+       console.log(event.target.value);
+       setNewNote(event.target.value);
+     };
+
+       const notesToShow = showAll
+         ? notes
+         : notes.filter((note) => note.important === true);
 
   return (
-    <>
-      <div style={{ color: "white" }}>{anecdotes[selected]}</div>
-      <h4 style={{ color: "white" }}>This one has {votes[selected]} votes;</h4>
-      <button onClick={randomizer}>Next anedote</button>
-      <button onClick={() => voter(selected)}>Vote</button>
-
-      <h3 style={{ color: "white" }}>The most voted one is:</h3>
-      <p style={{ color: "white" }}>
-        {anecdotes[mostVoted]}, has {Math.max(...votes)}.
-      </p>
-    </>
-  ); 
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
